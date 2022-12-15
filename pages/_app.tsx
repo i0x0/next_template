61f168c8a296e99954b707b137b8c0ba61233b77
TheHackerCoding <triniziah@gmail.com>
2022-12-14 22:01:17 -0500

    should be the only commit for now

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { createTheme, NextUIProvider } from '@nextui-org/react'
+
+const darkTheme = createTheme({
+  type: 'dark',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <NextUIProvider theme={darkTheme}>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  </>
 }
